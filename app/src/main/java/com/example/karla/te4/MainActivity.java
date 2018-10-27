0ebc5317818bae23b402d097dc3417a90cc1178e
Karla Rivera <karla.rivera24@gmail.com>
2018-10-26 23:50:54 -0600

    Solo se ejecuta local

--- app/src/main/java/com/example/karla/te4/MainActivity.java
+++ app/src/main/java/com/example/karla/te4/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.karla.te4;
 
-import android.support.constraint.ConstraintLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,26 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         main.main(new String[]{});
-        TextView txtMaze = (TextView) findViewById(R.id.maze);
+        TextView txtMaze = findViewById(R.id.maze);
         txtMaze.setText(lab);
+
     }
 
     public static void txtMazeSetText(String maze){
         lab = maze;
     }
 
-
-
-
-
-
-
-
-
-
-    //////
 
 }
 
